d1979e817b3d5728caa01ec927ce415a20ae2842
praketa <praketasxena@gmail.com>
2019-12-31 20:41:56 -0600

    - Second commit Praketa

---
+++ src/App/App.js
@@ -0,0 +1,25 @@
+import React,{Component} from 'react';
+import data from '../dummy-store';
+
+
+console.log(data);
+
+class App extends Component{
+  render(){
+
+    //STATE
+    
+    state ={};
+    
+    //CREATE ROUTES
+
+
+    return (<div>
+      {}
+    </div>)
+  
+
+}
+}
+
+export default App;
---
+++ src/noteCard/noteCard.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom';
+import './noteCard.css'
+
+
+class ControlBar extends Component {
+    render() {
+      return (
+        <div className="noteCard">
+            
+        </div>
+      );
+    }
+  }
+  
+  export default ControlBar;

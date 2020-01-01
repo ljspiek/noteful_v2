31066c9902881fdc897be3d4a3dbb2a02c91c352
praketa <praketasxena@gmail.com>
2020-01-01 11:17:40 -0600

    -notefullHome component added

--- src/App/App.js
+++ src/App/App.js
@@ -1,25 +1,32 @@
 import React,{Component} from 'react';
+
 import data from '../dummy-store';
+import NotefullHome from '../notefullHome/notefullHome';
 
 
 console.log(data);
+
+
 
 class App extends Component{
   render(){
 
     //STATE
 
-    state ={};
+    // state ={};
     
     //CREATE ROUTES
 
    
     //MAIN RENDER
-    return (<div>
-    {/* FOLDER NAVIGATION */}  
-    {/* HEADER FOR NOTEFUL HOME  */}
-    {/* MAIN CARDS */}
+    return ( <div>
+      <NotefullHome name="Noteful Home"/>
     </div>)
+    // return (<div>
+    // {/* FOLDER NAVIGATION */}  
+    // {/* HEADER FOR NOTEFUL HOME  */}
+    // {/* MAIN CARDS */}
+    // </div>)
   
 
 }
---
+++ src/notefullHome/notefullHome.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import './notefullHome.css';
+
+
+function NotefullHome(props){
+  
+    return ( 
+      <div className="nhm">
+        <h1>{props.name}</h1>
+      </div>
+   )
+  }
+
+export default NotefullHome; 
+
